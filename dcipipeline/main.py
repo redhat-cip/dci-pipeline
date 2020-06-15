92869818b2763399403e6b584de6c934b2594eda
ylamgarchal <yassine.lamgarchal@redhat.com>
2020-06-15 12:04:05 +0200

    pipeline.yml: refine the pipeline description
      - each stage is named and typed
      - the location correspond to the directory
        of an agent with expected files
      - the fallback_last_succeed flag will make to
        schedule a job on top of an already component
        that succeed before if the current failed
    
    main.py: separate the OCP stage from the CNFs
    stages
    
      - add run_ocp() and run_cnf()
      - the cnf accepts the ocp config in order to
        get access to the ocp configuration when
        running
    
    agents/:
    
      - the agents/ directory contains the agents code
      - agent.yml is the main playbook to call
      - dci_credentials.yml contains the DCI variables
        for authenticating against the dci server

--- dcipipeline/main.py
+++ dcipipeline/main.py
@@ -16,37 +16,106 @@
 
 import ansible_runner
 
+import os
+import sys
+import yaml
+
+
+def load_yaml_file(path):
+    with open(path, 'r') as file:
+        try:
+            return yaml.load(file, Loader=yaml.SafeLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit(1)
+
+
+def check_pipeline(pipeline):
+    def _check_agent_directory(path):
+        print('check agent directory at %s' % path)
+        pass
+
+
+def get_ocp_stage(pipeline):
+    for stage in pipeline:
+        if stage['type'] == 'ocp':
+            return dict(stage)
+    return None
+
+
+def get_cnf_stages(pipeline):
+    cnf_stages = []
+    for stage in pipeline:
+        if stage['type'] == 'cnf':
+            cnf_stages.append(dict(stage))
+    return cnf_stages
+
+
+def run_ocp(stage, dci_credentials, envvars):
+    # schedule job on topic
+    # run ocp playbook with job_info
+    # return job_info
+    print('running ocp stage: %s' % stage['name'])
+    envvars = dict(envvars)
+    envvars.update(dci_credentials)
+    run = ansible_runner.run(
+        private_data_dir="/home/yassine/dci/dci-pipeline/",
+        playbook='%s/agent.yml' % stage['location'],
+        verbosity=2,
+        envvars=envvars,
+        quiet=False)
+    print(run.stats)
+
+    return {
+        'ocp_config': {
+            'host': 'host.example.com',
+            'worker': 'worker.example.com'}}
+
+
+def run_cnf(stage, ocp_job_config, dci_credentials, envvars):
+    # schedule job on topic with
+    # ocp_job_config components
+    # run cnf playbook with ocp config
+    print('running cnf stage: %s' % stage['name'])
+    envvars = dict(envvars)
+    envvars.update(dci_credentials)
+    extravars = {}
+    extravars.update(ocp_job_config)
+    run = ansible_runner.run(
+        private_data_dir="/home/yassine/dci/dci-pipeline/",
+        playbook='%s/agent.yml' % stage['location'],
+        verbosity=2,
+        envvars=envvars,
+        extravars=extravars,
+        quiet=False)
+    print(run.stats)
+
 
 def main():
-    print('**** running ocp agent ****')
-    ocp_extravars = {
-        'DCI_CLIENT_ID': '',
-        'DCI_API_SECRET': '',
-        'DCI_CS_URL': 'https://api.distributed-ci.io/'
-    }
-    ocp_run = ansible_runner.run(
-        private_data_dir="/home/yassine/dci/dci-pipeline/",
-        playbook='/home/yassine/dci/dci-pipeline/dcipipeline/agents/openshift/agent.yml',
-        verbosity=3,
-        extravars=ocp_extravars)
-    print(ocp_run.stats)
 
-    print('\n\n**** running cnf agent ****')
-    cnf_extravars = {
-        'DCI_CLIENT_ID': '',
-        'DCI_API_SECRET': '',
-        'DCI_CS_URL': 'https://api.distributed-ci.io/',
-        'OCP_CONFIG': {'master': 'master.example.com', 'worker': 'worker.example.com'}
+    envvars = {
+        'ANSIBLE_CALLBACK_PLUGINS': os.getenv(
+            'ANSIBLE_CALLBACK_PLUGINS',
+            '/home/yassine/dci/dci-ansible/callback')
     }
-    cnf_run = ansible_runner.run(
-        private_data_dir="/home/yassine/dci/dci-pipeline/",
-        playbook='/home/yassine/dci/dci-pipeline/dcipipeline/agents/cnf/agent.yml',
-        verbosity=3,
-        extravars=cnf_extravars)
-    print(cnf_run.stats)
+    pipeline = load_yaml_file('/home/yassine/dci/dci-pipeline/dcipipeline/pipeline.yml')
+    check_pipeline(pipeline)
+
+    ocp_stage = get_ocp_stage(pipeline)
+    ocp_dci_credentials = load_yaml_file('%s/dci_credentials.yml' % ocp_stage['location'])
+    ocp_job_config = run_ocp(ocp_stage, ocp_dci_credentials, envvars)
+
+    cnf_stages = get_cnf_stages(pipeline)
+    for cnf_stage in cnf_stages:
+        dci_credentials = load_yaml_file('%s/dci_credentials.yml' % cnf_stage['location'])
+        dci_credentials = {
+            'DCI_CLIENT_ID': dci_credentials.get('DCI_CLIENT_ID'),
+            'DCI_API_SECRET': dci_credentials.get('DCI_API_SECRET'),
+            'DCI_CS_URL': dci_credentials.get('DCI_CS_URL')
+        }
+
+        run_cnf(cnf_stage, ocp_job_config, dci_credentials, envvars)
 
 
 if __name__ == '__main__':
-    #pipeline_file = sys.argv[1]
-
     main()
