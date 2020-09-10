e4bb01d983a01a85a024a1df9fd8ae204c942f07
Frederic Lepied <flepied@redhat.com>
2020-09-10 08:18:40 +0200

    WIP: add the queue command
    Change-Id: I25e81428997aa531d0dab791db6b3c7a035b788b

---
+++ dciqueue/schedule_cmd.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+
+'''
+'''
+
+import logging
+import os
+import sys
+
+from dciqueue import lib
+
+log = logging.getLogger(__name__)
+
+COMMAND = 'schedule'
+
+
+def register_command(subparsers):
+    parser = subparsers.add_parser(COMMAND, help='Schedule a command on a pool')
+    parser.add_argument('pool', help='Name of the pool')
+    parser.add_argument('cmd', nargs='*')
+    return COMMAND
+
+
+def execute_command(args):
+    cmd = ' '.join(args.cmd)
+    if '@RESOURCE' not in cmd:
+        sys.stderr.write('no @RESOURCE in command: %s\n' % cmd)
+        return 1
+        
+    seq = lib.inc_seq(args)
+
+    queuefile = os.path.join(args.top_dir, 'queue', args.pool, str(seq))
+    with open(queuefile, 'w') as f:
+        f.write(cmd)
+
+    return 0
+
+# schedule_cmd.py ends here
