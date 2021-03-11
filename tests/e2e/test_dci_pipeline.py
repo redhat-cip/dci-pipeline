ce1a9df02b655207d3446459cd41e5bb77a1b8e5
Frederic Lepied <flepied@redhat.com>
2021-03-11 10:43:53 +0100

    dci-pipeline: handle SIGTERM and SIGINT
    on SIGTERM or SIGINT, stop the ansible-runner job gracefully and let the process finish.
    
    Change-Id: Iaef6de0cf155b2ed0b955df823d11e1625cb06ac

--- tests/e2e/test_dci_pipeline.py
+++ tests/e2e/test_dci_pipeline.py
@@ -17,7 +17,7 @@ from dcipipeline.main import main, PIPELINE
 
 import os
 import requests
-
+import time
 
 TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.basename(__file__))))
 
@@ -86,3 +86,19 @@ def test_dci_pipeline_skip():
 def test_dci_pipeline_upgrade():
     rc = main(["dci-pipeline", p("upgrade-pipeline.yml")])
     assert rc == 0
+
+
+def test_dci_pipeline_sigterm():
+    jobs = get_jobs()
+    pid = os.fork()
+
+    if pid == 0:
+        os.system("cd %s; dci-pipeline dcipipeline/pipeline-pause.yml")
+    else:
+        time.sleep(10)
+        os.system("killall dci-pipeline")
+        time.sleep(10)
+
+        jobs2 = get_jobs()
+        assert len(jobs) + 1 == len(jobs2)
+        assert jobs2[0]["status"] == "error"
