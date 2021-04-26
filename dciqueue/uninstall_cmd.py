c6265535f960418a8e86f6270be82a295d16fe6b
Frederic Lepied <flepied@redhat.com>
2021-04-26 06:12:57 +0200

    dci-queue: add install and uninstall sub-commands
    Change-Id: I0f2a21c2ad03d07848ed7c7938ae4f635171c824

---
+++ dciqueue/uninstall_cmd.py
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2021 Red Hat, Inc.
+#
+# Author: Frederic Lepied <flepied@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+'''
+'''
+
+import logging
+import os
+
+from dciqueue import lib
+
+log = logging.getLogger(__name__)
+
+COMMAND = "uninstall"
+
+
+def register_command(subparsers):
+    parser = subparsers.add_parser(
+        COMMAND, help="Uninstall dci-queue"
+    )
+    parser.add_argument("pool", help="Name of the pool")
+    return COMMAND
+
+
+def execute_command(args):
+    if not lib.check_pool(args):
+        return 1
+
+    cmd = "env EDITOR='dci-queue remove-crontab %s' crontab -e" % args.pool
+    log.info("Editing crontab with: '%s'" % cmd)
+    os.system(cmd)
+
+
+# uninstall_cmd.py ends here
