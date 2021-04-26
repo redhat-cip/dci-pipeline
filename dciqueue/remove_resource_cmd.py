7d3637147a5874d4e0e2bd9a1dce111aa46fd71a
Frederic Lepied <flepied@redhat.com>
2021-04-26 07:06:15 +0200

    dci-queue remove-resource: add a mandatory reason argument
    Change-Id: If34cd53f9fbe8c0ac92201e17496edec42322f16

--- dciqueue/remove_resource_cmd.py
+++ dciqueue/remove_resource_cmd.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 Red Hat, Inc
+# Copyright (C) 2020-2021 Red Hat, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -16,6 +16,9 @@
 """
 """
 
+import datetime
+import errno
+import json
 import logging
 import os
 
@@ -30,6 +33,7 @@ def register_command(subparsers):
     parser = subparsers.add_parser(COMMAND, help="Remove a resource from a pool")
     parser.add_argument("pool", help="Name of the pool")
     parser.add_argument("name", help="Name of the resource")
+    parser.add_argument("reason", help="Reason of the removal")
     return COMMAND
 
 
@@ -40,8 +44,30 @@ def execute_command(args):
     for key in ("available", "pool"):
         path = os.path.join(args.top_dir, key, args.pool, args.name)
         if os.path.exists(path) or os.path.islink(path):
-            log.debug("Removing %s" % path)
+            log.debug("Removing %s (%s)" % (path, args.reason))
             os.unlink(path)
+
+    dir = os.path.join(args.top_dir, "reason", args.pool)
+    try:
+        os.makedirs(dir)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            pass
+        else:
+            raise
+
+    path = os.path.join(args.top_dir, "reason", args.pool, args.name)
+    with open(path, "w") as f:
+        json.dump(
+            {
+                "reason": args.reason,
+                "pool": args.pool,
+                "resource": args.name,
+                "date": str(datetime.datetime.now()),
+            },
+            f,
+        )
+
     return 0
 
 
